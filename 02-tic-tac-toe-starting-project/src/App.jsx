fbd96a6e3747d5118214be773b138f96f3eb2458
nxson2509HD <nxson2509.dev@gmail.com>
2024-06-28 17:58:48 +0700

    update gameplay

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import Player from "./components/Player/Player";
+import GameBoard from "./components/GameBoard/GameBoard";
+import Log from "./components/Log/Log";
 
 function App() {
-  
+  const [activePlayerSymbol, setActivePlayer] = useState("X");
+  function handleActivePlayer() {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X"
+    );
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            name="Player 1"
+            symbol="X"
+            isActive={activePlayerSymbol === "X"}
+          />
+          <Player
+            name="Player 2"
+            symbol="O"
+            isActive={activePlayerSymbol === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleActivePlayer}
+          activePlayerSymbol={activePlayerSymbol}
+        />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ 02-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol, isActive }) {
+  const [isEditing, setEditing] = useState(false);
+  const [nameChange, setNameChange] = useState(name);
+  function hanleClick() {
+    //setEditing(!isEditing); => schedule update in State
+    setEditing((editing) => {
+      return !editing;
+    });
+  }
+  function handleChangeName(event) {
+    setNameChange(event.target.value);
+  }
+
+  let playerName = <span className="player-name">{nameChange}</span>;
+  if (isEditing) {
+    playerName = (
+      <input
+        type="text"
+        required
+        value={nameChange}
+        onChange={handleChangeName}
+      />
+    );
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={hanleClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
