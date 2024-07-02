71b6b109ca929fd5080fec7b46668182a567f50f
nxson2509HD <nxson2509.dev@gmail.com>
2024-07-02 08:14:25 +0700

    update

--- 03-invest-calculator-project/src/App.jsx
+++ 03-invest-calculator-project/src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
+import Header from "./components/Header/Header";
+import UserInput from "./components/UserInput/UserInput";
+import Results from "./components/Results/Resuts.";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestmentL: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newvalue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results />
+    </>
+  );
 }
 
-export default App
+export default App;
