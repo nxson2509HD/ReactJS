b2ac95f2b6111c22728edcf1353ef00415bfc9f1
nxson2509HD <nxson2509.dev@gmail.com>
2024-07-06 16:45:01 +0700

    update

--- 04-styling-project/src/App.jsx
+++ 04-styling-project/src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
 
-import "./index.css";
-
-// don't change the Component name "App"
 export default function App() {
-  const clickHandler = () => {
-    console.log("Clicked!");
-  };
-
   return (
     <>
-      <h2>You're logged in!</h2>
-      <p>Welcome to your user profile!</p>
-      <button onClick={clickHandler}>Click me!</button>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
